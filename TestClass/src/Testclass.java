d53a2372a76893494b73cfa75052cb64abf87a6d
DELL <DELL@DESKTOP-FDAF352>
2018-07-26 19:52:20 +0530

    commit to flash

---
+++ TestClass/src/Testclass.java
@@ -0,0 +1,11 @@
+class TestClass1{
+	void m1(){
+		System.out.println("TestClass1");	
+	}
+}
+ class Testclass {
+	 
+	public static void main(String[] args) {
+		System.out.println("Testclass");	
+	}
+}
